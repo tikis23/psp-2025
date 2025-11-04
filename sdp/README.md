cf329cc29b7cccae3134e721620ca66605b49212
Julius Mieliauskas <julius.mieliauskas@gmail.com>
2025-11-04 19:20:26 +0200

    initial springboot project

---
+++ sdp/README.md
@@ -0,0 +1,3 @@
+# Software Design Project 2025
+
+This is a springboot project for the 'Software Design Project' in Vilnius University.
---
+++ sdp/src/test/java/vu/software_project/sdp/SdpApplicationTests.java
@@ -0,0 +1,13 @@
+package vu.software_project.sdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
