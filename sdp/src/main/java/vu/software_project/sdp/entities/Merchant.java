2952e045ebf300b01c5f0931c33f96f375401809
justas <justas.draksas@gmail.com>
2025-11-23 11:08:58 +0200

    Implement entities

---
+++ sdp/src/main/java/vu/software_project/sdp/entities/Merchant.java
@@ -0,0 +1,28 @@
+package vu.software_project.sdp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "merchants")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String address;
+
+    private String contactInfo;
+
+    @Column(nullable = false)
+    private Long ownerId; // foreign key to User
+}
+
---
+++ sdp/src/main/java/vu/software_project/sdp/entities/Service.java
@@ -0,0 +1,30 @@
+
+package vu.software_project.sdp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "services")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Long merchantId; // foreign key to Merchant
+
+    private String taxRateId; // NOJUS STUFF
+}
